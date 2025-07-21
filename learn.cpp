e0daabc363a30d1b9c143698a14b5ab369e7b81a
zekeliu <zekepku0926@outlook.com>
2025-07-21 10:45:25 +0800

    Update learn.cpp

--- learn.cpp
+++ learn.cpp
@@ -1,22 +1,22 @@
 //定义变量
 int x=10;
 float y=3.14;
-std::string name='Alice'; //#include <string>
+string name='Alice'; //#include <string>
 
 //向量
 #include <vector>
-std::vector<int> mylist={1,2,3,4,5};
+vector<int> mylist={1,2,3,4,5};
 mylist.push_back(6); //append
 int first=mylist[0]; //索引
 mylist.size();//大小
 
 //输入输出
 int x;std::cin>>x;
-std::cout<<x<<std::endl;//#include <isostream>
+cout<<x<<endl;//#include <isostream>
 
 //函数
 int func1(int a, int b){return a+b;} //返回整数
-void func2(int a, int b){std::cout<<a+b<<std::endl;} //不返回
+void func2(int a, int b){cout<<a+b<<endl;} //不返回
 
 //const修饰符
 void func(const int x) {} //x 不能被修改
@@ -30,7 +30,7 @@ void func1(int& x) {} //直接传x的引用，修改会改变原本的x
 void func2(int x) {} // 传值，修改不会改变x，但是效率低，需要复制一份
 
 //条件
-if (x>0) {std::cout<<x<<std::endl;} else{std::cout<<-x<<std::endl;}
+if (x>0) {cout<<x<<endl;} else{cout<<-x<<endl;}
 
 //for循环
 for (int i=1;i<5;i+=2) {std::cout<<i<<std::endl;}
